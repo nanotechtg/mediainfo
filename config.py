fb623fb17a58e307bbb137752c83002fd15a90bc
nanotechtg <link2nanotech@gmail.com>
2024-11-30 16:25:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Telegram Bot Configuration
+API_ID = ""  # Replace with your API ID
+API_HASH = ""  # Replace with your API hash
+BOT_TOKEN = ""  # Replace with your bot token
+
+# Media Info Path (Temporary storage for media files)
+MEDIAINFO_PATH = "Mediainfo/"  # Folder to store media files temporarily
